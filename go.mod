b2b25e7cd2acbc049fe625afaaea2bdd70ff9da5
Tobias Theel <tt@fino.digital>
2020-07-22 12:49:19 +0200

    add test data

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Nerzal/goflux
 go 1.14
 
 require (
+	github.com/pkg/errors v0.9.1
 	github.com/urfave/cli/v2 v2.2.0
 	gopkg.in/yaml.v2 v2.2.2
 )
--- pkg/kustomize/service.go
+++ pkg/kustomize/service.go
@@ -1,7 +1,13 @@
 package kustomize
 
+import (
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+)
+
 type Service interface {
-	FetchRessources(path string)
+	FetchRessources(path string) (error, []string)
 }
 
 type service struct{}
@@ -10,6 +16,21 @@ func NewService() Service {
 	return &service{}
 }
 
-func (service *service) FetchRessources(path string) {
+func (service *service) FetchRessources(path string) (error, []string) {
+	fileInfos, err := ioutil.ReadDir(path)
+	if err != nil {
+		return errors.Wrap(err, "could not fetch resourrces"), nil
+	}
 
+	var result []string
+
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		result = append(result, fileInfo.Name())
+	}
+
+	return nil, result
 }
