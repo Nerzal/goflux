85054d849ed2fb27a9b45006c0b71c4b55039183
Tobias Theel <tt@fino.digital>
2020-07-22 17:13:01 +0200

    fixed some linter errors

--- pkg/service/models.go
+++ pkg/service/models.go
@@ -1,5 +1,6 @@
 package service
 
+// Data is used as structure for service files
 type Data struct {
 	APIVersion string   `yaml:"apiVersion"`
 	Kind       string   `yaml:"kind"`
@@ -7,28 +8,33 @@ type Data struct {
 	Spec       Spec     `yaml:"spec"`
 }
 
+// Spec holds spec options
 type Spec struct {
 	Ports    []Port   `yaml:"ports"`
 	Selector Selector `yaml:"selector"`
 }
 
+// Selector holds selector options
 type Selector struct {
 	App       string `yaml:"app"`
 	Component string `yaml:"component"`
 }
 
+// Port holds port data
 type Port struct {
 	Port       int    `yaml:"port"`
 	TargetPort int    `yaml:"targetPort"`
 	Protocol   string `yaml:"protocol"`
 }
 
+// Metadata holds metadata
 type Metadata struct {
 	Name      string `yaml:"name"`
 	Namespace string `yaml:"namespace"`
 	Labels    Labels `yaml:"labels"`
 }
 
+// Labels holds label data
 type Labels struct {
 	App       string `yaml:"app"`
 	Component string `yaml:"component"`
