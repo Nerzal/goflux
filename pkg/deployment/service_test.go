f35dd083458fc6cc395d14ee9a859bdece7f1d9c
Tobias Theel <tt@fino.digital>
2020-07-23 13:39:24 +0200

    add deployment test

---
+++ pkg/deployment/service_test.go
@@ -0,0 +1,15 @@
+package deployment_test
+
+import (
+	"testing"
+
+	"github.com/Nerzal/goflux/pkg/deployment"
+)
+
+func TestDeployment_Create(t *testing.T) {
+	service := deployment.NewService()
+	err := service.Create("kycnow-api", "clarilab", "myCoolSecret", "../../test/deployment/deployment.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+}
